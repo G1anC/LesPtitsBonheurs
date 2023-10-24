ad8127e227aaa17654da49c2e03c2e6b06f6cfbb
Noah Steiniger <noah.steiniger@epitech.eu>
2023-10-24 09:38:07 +0200

    feat (navBar) : trying a new starting navbar

--- JS/handleNavBar.js
+++
@@ -1,9 +0,0 @@
-
-
-function handleNavBar()
-{
-    const navElement = document.getElementsByTagName("nav")[0];
-
-    navElement.dataset.status === "active" ? navElement.dataset.status = "inactive" : navElement.dataset.status = "active";
-    console.log(navElement);
-}
